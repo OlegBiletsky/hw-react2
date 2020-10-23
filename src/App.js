8cba0fbf931983c510ed6443021317d7820fee94
OlegBiletsky <ol.biletsky@gmail.com>
2020-10-23 17:01:14 +0300

    1 task done!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Display from './components/display/display';
 import Input from './components/input/input';
 
 function App(props) {
+    let str1 = "1. Створити класовий компонент в якому з допомогою стану компоненту всі дані з інпута в який юзер буде вводити тест - будуть в режимі реального часу відображатись в функціональному компоненті в h1 тегу.";
   return (
-    <Input/>
+      <>
+        <Display text={str1}/>
+        <Input text="oleh"/>
+      </>
   );
 }
 
--- src/components/input/input.js
+++ src/components/input/input.js
@@ -1,23 +1,32 @@
 import React from 'react';
-import Display from './display/display';
+import Display from '../display/display';
 
 class Input extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            text: this.props.text
+            text: ""
         }
+
+        this.handleChange = this.handleChange.bind(this);
     }
+
+    handleChange(e) {
+        this.setState({text: e.target.value})
+    }
+
+
+
     render() {
         return(
-            <div>
+            <div> 
                 <input
-                    // type="text"
-                    // onChange = {()=>{}}
-                    // value = {this.state.name}
-                    
+                    type="text"
+                    onChange = {this.handleChange}
+                    value = {this.state.text}
+                    placeholder="write some text"
                 />
-                <Display text={this.props.text}/>
+                <Display text={this.state.text}/>
             </div>
         )
     }
