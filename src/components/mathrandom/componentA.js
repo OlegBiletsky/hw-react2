dca9f6add68047fd1ff46adec0072c578a89c47a
OlegBiletsky <ol.biletsky@gmail.com>
2020-10-24 00:09:20 +0300

    DONE!

--- src/components/mathrandom/componentA.js
+++ src/components/mathrandom/componentA.js
@@ -6,27 +6,31 @@ class ComponentA extends React.Component {
         super();
         this.state = {
             multiplyNumber: 1000,
+            generatedRandomNumber: 0,
             resultNumber: 0,
         }
         this.generateRandomNumber = this.generateRandomNumber.bind(this);
         this.multiplyAndSetState = this.multiplyAndSetState.bind(this);
     }
 
-    generateRandomNumber() {
+    generateRandomNumber() {/*функція яка генерує число */
         let generatedRandomNumber = Math.random().toFixed(6);
-        console.log(generatedRandomNumber);
+        console.log(generatedRandomNumber, 'its generatedRandomNumber in generateRandomNumber()');
         return generatedRandomNumber;
     }
-    multiplyAndSetState(n = 2) {
-        let m = 2 * this.state.multiplyNumber;
+    multiplyAndSetState() {/*функція яка множить число на 1000 */
+        let n = this.generateRandomNumber();
+        let m = n * this.state.multiplyNumber;
+        
         console.log(n, 'it is n');
         console.log(m, 'it is m');
         console.log(this.state.multiplyNumber, 'this.state.multiplyNumber');
-        console.log(this.state.resultNumber,'this.state.resultNumber');
+        console.log(this.state.resultNumber,'this.state.resultNumber BEFORE');
         
         
-        
-        this.setState({resultNumber: m})
+        this.setState({resultNumber: m, generatedRandomNumber: n})/*передаю у стейт обидва числа */
+
+        console.log(this.state.resultNumber,'this.state.resultNumber AFTER');/*пробую вивести у консоль числа із стейту -?? */
     }
     
     render() {
